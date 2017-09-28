c93cd0b3169df136a5423cd676b4d99273b8d4f8
makslixa4 <makslixa4@gmail.com>
2017-09-28 13:02:05 +0300

    first add
    add files

---
+++ script/myscript.js
@@ -0,0 +1,41 @@
+﻿var slideIndex = 1;
+showSlides (slideIndex);
+
+function plusSlides(n)
+{
+	showSlides(slideIndex +=n);
+}
+
+
+function showSlides(n)
+{
+	var i;
+	var slides = document.getElementsByClassName("MySlides");
+
+	if(n>slides.length)
+	{
+		slideIndex=1;
+	}
+	if(n<1)
+	{
+		slideIndex = slides.length;
+	}
+
+	for(i=0;i<slides.length;i++)
+	{
+		slides[i].style.display = "none";
+	}
+
+	slides[slideIndex-1].style.display = "block";
+}
+
+var items = document.getElementsByClassName("item");
+var i;
+for (var i = 0; i < items.length; i++) {
+	items[i].onclick = function()
+	{
+		this.classList.toggle("active");
+		this.nextElementSibling.classList.toggle("show");
+		return false;
+	}
+}
